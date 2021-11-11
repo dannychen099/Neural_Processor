361b909405523060daf067a866eec2b19405b9f9
dannychen099 <dannychen099@gmail.com>
2021-11-11 14:45:19 -0600

    testbench update

--- ReLU_tb.v
+++ ReLU_tb.v
@@ -1,9 +1,7 @@
 `timescale 1ns / 1ns
 
 module ReLU_tb();
-
     reg [80 - 1 : 0] in = {8'd2, 8'd1, 8'd7, 8'd20, 8'd17, 8'd13, 8'd15, 8'd6, 8'd7 , 8'd9};
-
     wire [3 : 0] max_val;
     reg enable = 1'b1;
     ReLU ReLU(
@@ -11,7 +9,6 @@ module ReLU_tb();
         .enable(enable),
         .max_val(max_val)
     );
-
     initial begin
         #100;
         $stop;
