5c0c2ea3ac34ed145f847b6089b88ce6e349fe2c
Stephen Matz <smatz46@gmail.com>
2021-11-29 13:49:09 -0600

    Added multi-channel and multi-filter support

--- model/c/README.md
+++ model/c/README.md
@@ -1,1 +1,38 @@
 # ETRI eyeriss_v1 reference code
+
+
+## MATLAB Model with RS Dataflow
+The expected ofmap with a 5x5x2 ifmap and 3x3x2 filter is a 3x3x2 ofmap. Using the following example, the expected calculation is:
+```Matlab
+>> ifmap1 = [0 1 2 3 4; 1 2 3 4 5; 2 3 4 5 6; 3 4 5 6 7; 4 5 6 7 8];
+>> ifmap2 = ifmap1 + 1
+
+ifmap2 =
+
+1     2     3     4     5
+2     3     4     5     6
+3     4     5     6     7
+4     5     6     7     8
+5     6     7     8     9
+
+>> ofmap1 = imfilter(ifmap1, filter1) + imfilter(ifmap2, filter1);
+>> ofmap2 = imfilter(ifmap1, filter2) + imfilter(ifmap2, filter2);
+>> ofmap1(2:end-1, 2:end-1)     % Display the valid convolution data
+
+ans =
+
+114   150   186
+150   186   222
+186   222   258
+
+>> ofmap2(2:end-1, 2:end-1)     % Display the valid convolution data
+
+ans =
+
+159   213   267
+213   267   321
+267   321   375
+```
+
+## C++ Model with the RS Dataflow
+The MATLAB model can also be implemented in C++, and the same results are obtained.
--- model/c/main.cpp
+++ model/c/main.cpp
@@ -11,20 +11,39 @@
 #define STRIDE          1
 #define OFMAP_WIDTH     (IFMAP_WIDTH - FILTER_WIDTH + STRIDE)/STRIDE
 #define OFMAP_HEIGHT    (IFMAP_HEIGHT - FILTER_HEIGHT + STRIDE)/STRIDE
-#define CHANNEL         1
+#define IFMAP_CHANNELS  2
+#define FILTER_CHANNELS 2
+#define OFMAP_CHANNELS  FILTER_CHANNELS
 
 using namespace std;
 
 // Local memory banks
-double filter[FILTER_WIDTH][FILTER_HEIGHT]  = { {0, 1, 2},
-                                                {1, 2, 3},
-                                                {2, 3, 4}   };
-double ifmap[IFMAP_WIDTH][IFMAP_HEIGHT] = { {0, 1, 2, 3, 4},
-                                            {1, 2, 3, 4, 5},
-                                            {2, 3, 4, 5, 6},
-                                            {3, 4, 5, 6, 7},
-                                            {4, 5, 6, 7, 8} };
-double ofmap[OFMAP_WIDTH][OFMAP_HEIGHT];
+double filter[FILTER_CHANNELS][FILTER_WIDTH][FILTER_HEIGHT] = {
+    {   {0, 1, 2},          // Filter channel 1
+        {1, 2, 3},
+        {2, 3, 4}
+    },
+    {
+        {1, 2, 3},          // Filter channel 2
+        {2, 3, 4},
+        {3, 4, 5}
+    }
+};
+double ifmap[IFMAP_CHANNELS][IFMAP_WIDTH][IFMAP_HEIGHT] = {
+    {   {0, 1, 2, 3, 4},    // ifmap channel 1
+        {1, 2, 3, 4, 5},
+        {2, 3, 4, 5, 6},
+        {3, 4, 5, 6, 7},
+        {4, 5, 6, 7, 8}
+    },
+    {   {1, 2, 3, 4, 5},    // ifmap channel 2
+        {2, 3, 4, 5, 6},
+        {3, 4, 5, 6, 7},
+        {4, 5, 6, 7, 8},
+        {5, 6, 7, 8, 9}
+    }
+};
+double ofmap[OFMAP_CHANNELS][OFMAP_WIDTH][OFMAP_HEIGHT];
 double zero = 0;
 
 typedef vector<PE_unit> PE_1D;
@@ -53,123 +72,134 @@ int main()
         }
     }
 
-    // Calculate each ofmap row
-    for (int ofmapRow = 0; ofmapRow < OFMAP_HEIGHT; ofmapRow++) {
+    for (int filterChannel = 0; filterChannel < FILTER_CHANNELS; filterChannel++) {
 
-        // Reset the psums in each PE to zero
-        for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
-            for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
-                PE_array[pe_y][pe_x].set_psum();
+        // Calculate each ofmap row
+        for (int ofmapRow = 0; ofmapRow < OFMAP_HEIGHT; ofmapRow++) {
+
+            // Reset the psums in each PE to zero
+            for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
+                for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
+                    PE_array[pe_y][pe_x].set_psum();
+                }
             }
-        }
 
-        // Cycle through each filter row. Going through all rows completes 
-        // the sliding-window convolution.
-        for (int filterRow = 0; filterRow < FILTER_HEIGHT; filterRow++) {
-
-            //-----------------------------------------------------------------
-            //  Configure filter
-            //-----------------------------------------------------------------
-            printf("\n----- Set PE_array Filter ID (hexadecimal addresses) -----\n");
-            // Over each PE row...
-            for (int pe_y = 0; pe_y < FILTER_WIDTH; pe_y++) {
-                // Over each PE column...
-                for (int pe_x = 0; pe_x < OFMAP_WIDTH; pe_x++) {
-                    /* Set the filter ID in each PE row to the same value.
-                        For filter with width N:
-                        1 1 1 ... 1
-                        2 2 2 ... 2
-                          .   .
-                          .    .
-                          .     .
-                        N N N ... N
+                // Cycle through each filter row. Going through all rows completes 
+                // the sliding-window convolution.
+                for (int filterRow = 0; filterRow < FILTER_HEIGHT; filterRow++) {
                         
-                       Filter IDs are the same across each PE row.
-                    */
-                    PE_array[pe_y][pe_x].set_filter_id(&filter[filterRow][pe_y]);
-                    printf("%X ", PE_array[pe_y][pe_x].filter_id);
+                    //-----------------------------------------------------------------
+                    //  Configure filter row
+                    //-----------------------------------------------------------------
+                    printf("\n----- Set PE_array Filter ID (hexadecimal addresses) -----\n");
+
+                    // Over each PE row...
+                    for (int pe_y = 0; pe_y < FILTER_WIDTH; pe_y++) {
+                        // Over each PE column...
+                        for (int pe_x = 0; pe_x < OFMAP_WIDTH; pe_x++) {
+                            /* Set the filter ID in each PE row to the same value.
+                                For filter with width N:
+                                1 1 1 ... 1
+                                2 2 2 ... 2
+                                  .   .
+                                  .    .
+                                  .     .
+                                N N N ... N
+                                
+                               Filter IDs are the same across each PE row.
+                            */
+                            PE_array[pe_y][pe_x].set_filter_id(&filter[filterChannel][filterRow][pe_y]);
+                            printf("%X ", PE_array[pe_y][pe_x].filter_id);
+                        }
+                        printf("\n");
+                    }
+
+                    printf("\n----- Set and Display PE_array Filter Value -----\n");
+                    for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
+                        for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
+                            PE_array[pe_y][pe_x].set_filter();
+                            printf("%f ", PE_array[pe_y][pe_x].filter);
+                        }
+                        printf("\n");
+                    }
+
+                //-----------------------------------------------------------------
+                //  Configure each ifmap
+                //-----------------------------------------------------------------
+                printf("\n----- Load another ifmap -----\n");
+                for (int ifmapChannel = 0; ifmapChannel < IFMAP_CHANNELS; ifmapChannel++) {
+
+                    printf("\n----- Set PE_array Ifmap ID (hexadecimal addresses) -----\n");
+                    // Over each PE row...
+                    for (int pe_y = 0; pe_y < FILTER_WIDTH; pe_y++) {
+                        // Over each PE column...
+                        for (int pe_x = 0; pe_x < OFMAP_WIDTH; pe_x++) {
+                            /* Share ifmap values diagonally across the PE grid
+                                For filter with width N and ofmap width M:
+                                1 2 3 ... M 
+                                2 3 4 ... M+1
+                                3 4 5 ... M+2
+                                 ...
+                               This is essentially a symmetric matrix
+                            */
+                            PE_array[pe_y][pe_x].set_ifmap_id(&ifmap[ifmapChannel][ofmapRow+filterRow][pe_y+pe_x]);
+                            printf("%X ", PE_array[pe_y][pe_x].ifmap_id);
+                        }
+                        printf("\n");
+                    }
+
+                    printf("\n----- Set and display PE_array ifmap value -----\n");
+                    for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
+                        for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
+                            PE_array[pe_y][pe_x].set_ifmap();
+                            printf("%f ", PE_array[pe_y][pe_x].ifmap);
+                        }
+                        printf("\n");
+                    }
+
+
+                    //-----------------------------------------------------------------
+                    //  Calculate psums for filter convolution on current row
+                    //-----------------------------------------------------------------
+                    printf("\n----- psum %d of ofmap row %d -----\n", filterRow, ofmapRow);
+                    for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
+                        for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
+                            PE_array[pe_y][pe_x].single_line_mac();
+                            printf("%f ", PE_array[pe_y][pe_x].psum);
+                        }
+                        printf("\n");
+                    }
                 }
-                printf("\n");
             }
 
-            printf("\n----- Set and Display PE_array Filter Value -----\n");
-            for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
-                for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
-                    PE_array[pe_y][pe_x].set_filter();
-                    printf("%f ", PE_array[pe_y][pe_x].filter);
+            printf("\n----- Accumulate psums upwards-----\n");
+            for (int i = 0; i < FILTER_HEIGHT; i++) {
+                for (int pe_y = PE_ARRAY_HEIGHT-1; pe_y > 0; pe_y--) {
+                    for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
+                        double bottom_psum = PE_array[pe_y][pe_x].psum;
+                        PE_array[pe_y-1][pe_x].single_line_acc(bottom_psum);
+                        PE_array[pe_y][pe_x].psum = 0;
+                    }
                 }
-                printf("\n");
             }
 
-            //-----------------------------------------------------------------
-            //  Configure ifmap
-            //-----------------------------------------------------------------
-            printf("\n----- Set PE_array Ifmap ID (hexadecimal addresses) -----\n");
-            // Over each PE row...
-            for (int pe_y = 0; pe_y < FILTER_WIDTH; pe_y++) {
-                // Over each PE column...
-                for (int pe_x = 0; pe_x < OFMAP_WIDTH; pe_x++) {
-                    /* Share ifmap values diagonally across the PE grid
-                        For filter with width N and ofmap width M:
-                        1 2 3 ... M
-                        2 3 4 ... M+1
-                        3 4 5 ... M+2
-                         ...
-                       This is essentially a symmetric matrix
-                    */
-                    PE_array[pe_y][pe_x].set_ifmap_id(&ifmap[ofmapRow+filterRow][pe_y+pe_x]);
-                    printf("%X ", PE_array[pe_y][pe_x].ifmap_id);
-                }
-                printf("\n");
-            }
-
-            printf("\n----- Set and display PE_array ifmap value -----\n");
-            for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
-                for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
-                    PE_array[pe_y][pe_x].set_ifmap();
-                    printf("%f ", PE_array[pe_y][pe_x].ifmap);
-                }
-                printf("\n");
-            }
-
-            //-----------------------------------------------------------------
-            //  Calculate psums for filter convolution on current row
-            //-----------------------------------------------------------------
-            printf("\n----- psum %d of ofmap row %d -----\n", filterRow, ofmapRow);
-            for (int pe_y = 0; pe_y < PE_ARRAY_HEIGHT; pe_y++) {
-                for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
-                    PE_array[pe_y][pe_x].single_line_mac();
-                    printf("%f ", PE_array[pe_y][pe_x].psum);
-                }
-                printf("\n");
+            for (int j = 0; j < OFMAP_WIDTH; j++) {
+                ofmap[filterChannel][ofmapRow][j] = PE_array[0][j].psum;
+                printf("%f ", ofmap[filterChannel][ofmapRow][j]);
             }
         }
+    }
 
-        printf("\n----- Accumulate psums upwards-----\n");
-        for (int i = 0; i < FILTER_HEIGHT; i++) {
-            for (int pe_y = PE_ARRAY_HEIGHT-1; pe_y > 0; pe_y--) {
-                for (int pe_x = 0; pe_x < PE_ARRAY_WIDTH; pe_x++) {
-                    double bottom_psum = PE_array[pe_y][pe_x].psum;
-                    PE_array[pe_y-1][pe_x].single_line_acc(bottom_psum);
-                    PE_array[pe_y][pe_x].psum = 0;
-                }
-            }
-
-        }
-
-        for (int j = 0; j < OFMAP_WIDTH; j++) {
-            ofmap[ofmapRow][j] = PE_array[0][j].psum;
-            printf("%f ", ofmap[ofmapRow][j]);
-        }
-
-        printf("\n----- Display final ofmap -----\n");
+    printf("\n----- Display final ofmap -----\n");
+    for (int ofmapChannel = 0; ofmapChannel < OFMAP_CHANNELS; ofmapChannel++) {
+        printf("\n----- ofmap channel %d-----\n", ofmapChannel);
         for (int i = 0; i < OFMAP_HEIGHT; i++) {
             for (int j = 0; j < OFMAP_WIDTH; j++) {
-                printf("%f ", ofmap[i][j]);
+                printf("%f ", ofmap[ofmapChannel][i][j]);
             }
             printf("\n");
         }
-
+        printf("\n");
     }
 	return 0;
 }
